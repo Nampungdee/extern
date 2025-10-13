10bee5947391880d26d7dc010ac25d5e5defa1d3
น้ำผึ้ง <Nampungdee5555@gmail.com>
2025-10-13 22:43:16 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,12 @@
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbx9f2QXQGpiV03OfUAB-83azoywX71yb65k1d8z3QEp-Reh-MORTrJGheWyyJWYJtW9/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw9sfQAe46ClxFehKXe2nG7992XC0dKwqWfbhD79NUUS-2jHj4Qcvdz4ej60WJo99Y_/exec';
+//LIFF_ID
+    const LIFF_ID = '2007868023-VD36zPx6'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2007868023-z4ZY4g9Y'; // LIFF ID history
+    const LIFF_REGISTER = '2007868023-QvDrAKlr'; // LIFF ID register
+
+
+
+
+

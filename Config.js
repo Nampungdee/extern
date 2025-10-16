1909ff62fd75920faee505997a1a8dcf23aa8c95
น้ำผึ้ง <Nampungdee5555@gmail.com>
2025-10-16 14:43:46 +0700

    Change Web App URLs in Config.js
    Updated Web App URLs for member and general access.

--- Config.js
+++ Config.js
@@ -1,10 +1,11 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxd3YOGktbur0pNG93goHZV59HvMaaP84I1aW31BU0xcwEdA7uVf46-e8oXHZBrYpl36Q/exec';
-    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxW1XlY4sUVgQrbmAJdusho4VTMS56y1YlD_jfeKJiL4IAmSmHgYB1NavC2fwfk74tp/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyAwJmj9cvP1Nxvt2fOvWCU848_YSe1RDHHPCxIJ-e9GW0dBNCDB07rB7lA5JOkRRJnCA/exec';
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbycVrPxINpIRDpjuFzVk91nW6Pl5bPJlBgaR_trnHE2xHoXjSEA_F1SG1NG7YY1pHQS/exec';
 //LIFF_ID
     const LIFF_ID = '2008276424-eb6wK5OJ'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008276424-07KN1Eqz'; // LIFF ID history
     const LIFF_REGISTER = '2008276424-k7E1rAnP'; // LIFF ID register
+
 
 
 

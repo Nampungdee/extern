c7ab6f5bc182c20755a3eb13c133016f9db0e6ed
น้ำผึ้ง <Nampungdee5555@gmail.com>
2026-06-24 14:18:16 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,6 @@
     const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxz9plWWa63-PG5YblEnlmEHc1qdZdCpbkABcqk-SkNYqFCzt_qNXaNv5Gkvmui7sFo/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyKWZ6emtV4m-20zBukwHHGt7Jdt-uIpQJfVkMn_PtT_FneITK4B7qzG-hw2HBsgx2LXg/exec';
 //LIFF_ID
-    const LIFF_ID = '2008276424-eb6wK5OJ'; // LIFF ID Checkin
-    const LIFF_HISTORY = '2008276424-07KN1Eqz'; // LIFF ID history
-    const LIFF_REGISTER = '2008276424-k7E1rAnP'; // LIFF ID register
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const LIFF_ID = '2007656229-B9EAzV8v'; // LIFF ID Checkin
+    const LIFF_HISTORY = '2007656229-8nnlMGYV'; // LIFF ID history
+    const LIFF_REGISTER = '2007656229-kRNMD5gB'; // LIFF ID register
